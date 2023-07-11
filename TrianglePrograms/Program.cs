50f32e101f5aadeff81fdb60850b6ecd806d6c6c
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-11 15:39:22 +0530

    [Riya] Add : Implemented Alphabet Triangle

--- TrianglePrograms/Program.cs
+++ TrianglePrograms/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TrianglePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangles triangles = new Triangles();
+            triangles.AlphabetTriangle();
+
+        }
+    }
+}
+
--- TrianglePrograms/Triangles.cs
+++ TrianglePrograms/Triangles.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace TrianglePrograms
 {
-    internal class Triangles
+    public class Triangles
     {
+        public void AlphabetTriangle()
+        {
+            char ch = 'A';
+            int i, j, k, m;
+            Console.WriteLine("Enter the limit :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = n; j >= i; j--)
+                    Console.Write(" ");
+                for (k = 1; k <= i; k++)
+                    Console.Write(ch++);
+                ch--;
+                for (m = 1; m < i; m++)
+                    Console.Write(--ch);
+                Console.Write("\n");
+                ch = 'A';
+            }
+        }
     }
 }
